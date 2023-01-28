d3b731055a90cb1d6ae4c4359bea691667033bee
spelt-coder <mannclement6@gmail.com>
2023-01-28 17:43:21 +0000

    create testproject

---
+++ src/logic/SyntaxException.java
@@ -0,0 +1,15 @@
+package logic;
+
+public class SyntaxException extends RuntimeException {
+	
+	public SyntaxException()
+	{
+		super("Syntax error");
+	}
+	
+	public SyntaxException(String message)
+	{
+		super(message);
+	}
+
+}
